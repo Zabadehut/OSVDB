ba69409ab1b43ac634d1033ad203b332078a70dd
Zabadehut <zaba88@hotmail.fr>
2024-04-14 14:07:22 +0200

    Memory_Opt
    mergeContiguousBlocks(free_block* block)...

--- kernel/src/memory.cpp
+++ kernel/src/memory.cpp
@@ -1,45 +1,36 @@
-
 #include "memory.h"
 
-uint64_t GetMemorySize(EFI_MEMORY_DESCRIPTOR* mMap, uint64_t mMapEntries, uint64_t mMapDescSize){
-
+uint64_t GetMemorySize(EFI_MEMORY_DESCRIPTOR* mMap, uint64_t mMapEntries, uint64_t mMapDescSize) {
     static uint64_t memorySizeBytes = 0;
     if (memorySizeBytes > 0) return memorySizeBytes;
-
+    uint64_t descSizeTimesI = mMapDescSize; // Add mMapDescSize after each iteration
     for (int i = 0; i < mMapEntries; i++){
-        EFI_MEMORY_DESCRIPTOR* desc = (EFI_MEMORY_DESCRIPTOR*)((uint64_t)mMap + (i * mMapDescSize));
+        EFI_MEMORY_DESCRIPTOR* desc = (EFI_MEMORY_DESCRIPTOR*)((uint64_t)mMap + descSizeTimesI);
         memorySizeBytes += desc->numPages * 4096;
+        descSizeTimesI += mMapDescSize;
     }
-
     return memorySizeBytes;
-
 }
 
-void memset(void* start, uint8_t value, uint64_t num){
+void memset(void* start, uint8_t value, uint64_t num) {
     for (uint64_t i = 0; i < num; i++){
         *(uint8_t*)((uint64_t)start + i) = value;
     }
 }
 
-void* memcpy(void* dst, const void* src, uint16_t num)
-{
+void* memcpy(void* dst, const void* src, uint16_t num) {
     uint8_t* u8Dst = (uint8_t*)dst;
     const uint8_t* u8Src = (const uint8_t*)src;
-
-    for (uint16_t i = 0; i < num; i++)
-        u8Dst[i] = u8Src[i];
-
+    while (num--)
+        *u8Dst++ = *u8Src++;
     return dst;
 }
 
-int memcmp(const void* ptr1, const void* ptr2, uint16_t num)
-{
+int memcmp(const void* ptr1, const void* ptr2, uint16_t num) {
     const uint8_t* u8Ptr1 = (const uint8_t*)ptr1;
     const uint8_t* u8Ptr2 = (const uint8_t*)ptr2;
-
-    for (uint16_t i = 0; i < num; i++)
-        if (u8Ptr1[i] != u8Ptr2[i])
+    while (num--)
+        if (*u8Ptr1++ != *u8Ptr2++)
             return 1;
-
     return 0;
 }
--- kernel/src/memory/standardHeap.cpp
+++ kernel/src/memory/standardHeap.cpp
@@ -9,10 +9,22 @@ void *sbrk(intptr_t incr)
     return (void*) old_break;
 }
 
+void mergeContiguousBlocks(free_block* block) {
+    while(block && block->next) {
+        if ((char*)block + sizeof(free_block) + block->size == (char*)block->next) {
+            block->size += sizeof(free_block) + block->next->size;
+            block->next = block->next->next;
+        }
+        block = block->next;
+    }
+}
+
+
 void* standardMalloc(size_t size) {
     size = (size + sizeof(size_t) + (align_to - 1)) & ~ (align_to - 1);
     free_block* block = free_block_list_head.next;
     free_block** head = &(free_block_list_head.next);
+
     while (block != 0) {
         if (block -> size >= size) {
             *head = block->next;
@@ -25,6 +37,11 @@ void* standardMalloc(size_t size) {
     block = (free_block*)sbrk(size);
     block -> size = size;
 
+    // Adding the new block to the head of the free block list,
+    // to make it quickly available for the next allocation request.
+    block -> next = free_block_list_head.next;
+    free_block_list_head.next = block;
+
     return ((char*)block) + sizeof(size_t);
 }
 
@@ -32,4 +49,9 @@ void standardFree(void* ptr) {
     free_block* block = (free_block*)(((char*)ptr) - sizeof(size_t));
     block -> next = free_block_list_head.next;
     free_block_list_head.next = block;
+    // Add code to merge the contiguous blocks in the free block list.
+    // If the immediate neighbors of the block being freed are also free,
+    // they can be merged to form a larger block. This reduces fragmentation.
+    mergeContiguousBlocks(free_block_list_head.next);
 }
+
